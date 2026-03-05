3cc3c1966555b840ac80c90a130b37e69d519a8b
Deepthi <deepthidrao15@gmail.com>
2026-03-05 21:04:38 +0530

    Initial commit for milestone1

---
+++ download_rocket_launcher.py
@@ -0,0 +1,61 @@
+import json
+import pathlib
+from datetime import datetime, timedelta
+import requests
+import airflow
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
+
+dag = DAG(
+    dag_id="download_rocket_launcher",
+    start_date=datetime.now() - timedelta(days=14),
+    schedule="0 6 * * 1,3,5",   # Monday Wednesday Friday 6 AM
+    catchup=False
+)
+
+download_launches = BashOperator(
+    task_id="download_launchers",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
+    dag=dag,
+)
+
+def _get_pictures():
+    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
+
+    with open("/tmp/launches.json") as f:
+        launches = json.load(f)
+
+    image_urls = [launch["image"] for launch in launches["results"] if launch["image"]]
+
+    for image_url in image_urls:
+        try:
+            response = requests.get(image_url, timeout=30)
+
+            image_filename = image_url.split("/")[-1]
+            target_file = f"/tmp/images/{image_filename}"
+
+            with open(target_file, "wb") as img_file:
+                img_file.write(response.content)
+
+            print(f"Downloaded {image_url} to {target_file}")
+
+        except requests.exceptions.MissingSchema:
+            print(f"{image_url} appears to be an invalid URL.")
+
+        except requests.exceptions.ConnectionError:
+            print(f"Could not connect to {image_url}.")
+
+get_pictures = PythonOperator(
+    task_id="get_pictures",
+    python_callable=_get_pictures,
+    dag=dag
+)
+
+notify = BashOperator(
+    task_id="notify",
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    dag=dag,
+)
+
+download_launches >> get_pictures >> notify
